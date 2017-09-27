369f45aa4cdea0573541f441554ab5c2abbc05b7
Ross Waycaster <rosswaycaster@me.com>
2017-09-27 16:45:56 -0500

    sessions

--- server/api/index.js
+++ server/api/index.js
@@ -38,6 +38,7 @@ router.post('/signup', (req, res) => {
     if (err) {
       res.status(401).send({err: err});
     } else {
+      req.session.user_id = user._id;
       res.send(user);
     }
   })(req, res);
@@ -48,6 +49,7 @@ router.post('/login', (req, res) => {
     if (err) {
       res.status(401).send({err: err});
     } else {
+      req.session.user_id = user._id;
       res.send(user);
     }
   })(req, res);
