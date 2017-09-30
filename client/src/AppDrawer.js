61d66a81c33af9a01160d559bacf03b575abb339
Pete Givens <pgivens@gmail.com>
2017-09-30 14:27:12 -0500

    added profile section on menu drawer

--- client/src/AppDrawer.js
+++ client/src/AppDrawer.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 import Close from 'material-ui/svg-icons/navigation/close';
 import { Link } from 'react-router-dom';
+import ProfileMenuSection from './ProfileMenuSection';
+import muiThemeable from 'material-ui/styles/muiThemeable';
 
-const AppDrawer = ({open, toggleDrawer, currentUser}) => {
+const AppDrawer = ({open, toggleDrawer, currentUser, muiTheme}) => {
   return (
     <div>
       <Drawer open={open} onClick={toggleDrawer}>
-        <MenuItem rightIcon={<Close />} onClick={toggleDrawer}>
+        <MenuItem style={{color: muiTheme.palette.accent1Color}} rightIcon={<Close color={muiTheme.palette.accent1Color}/>} onClick={toggleDrawer}>
           Close
         </MenuItem>
+        <Divider />
+        <ProfileMenuSection currentUser={currentUser} />
+        <Divider />
         <MenuItem onClick={toggleDrawer}>
           <Link to={'/'+currentUser.username}>My Lists</Link>
         </MenuItem>
@@ -21,4 +27,4 @@ const AppDrawer = ({open, toggleDrawer, currentUser}) => {
   );
 }
 
-export default AppDrawer;
+export default muiThemeable()(AppDrawer);
