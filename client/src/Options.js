23eba1a8601625dfc1233b2d1ade4a2ae5a0e09a
Ben Collins <ghostcoder8@hotmail.com>
2017-11-16 14:15:09 -0600

    Renders basic options page but is not functional yet

--- client/src/Options.js
+++ client/src/Options.js
@@ -6,18 +6,72 @@ import TextField from 'material-ui/TextField';
 import axios from 'axios';
 import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
-
+import Dropzone from 'react-dropzone';
 
 export default class Options extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      emailInput: ''
+      emailInput: '',
+      fileReceived: false,
+      fileName: '',
+      files: null,
     };
 
+    this.uploadFile = () => {
+      const files = this.state.files;
 
+      //if the user has added a file
+      if (files) {
+        /* map over all of the images, upload them, and post them to db
+        (right now there is only 1 image, but can be
+        changed later to accept and render multiple images) */
+        files.map(file => {
+          // Initial FormData
+          const formData = new FormData();
+          formData.append("file", file);
+          formData.append("tags", `getmeagift`);
+          formData.append("upload_preset", "n5n2w26w"); //preset is with account
+          formData.append("api_key", "365845311351591"); //key is based on account
+          formData.append("timestamp", (Date.now() / 1000) | 0);
+
+          // Make an AJAX upload request using Axios
+          // The url is provided by cloudinary
+          return axios.post("https://api.cloudinary.com/v1_1/getmeagiftlegacy/image/upload", formData, {
+            headers: { "X-Requested-With": "XMLHttpRequest" },
+          })
+          .then(response => {
+            const data = response.data;
+            //url of image in cloudinary
+            console.log(data);
+            const fileURL = data.secure_url
+            console.log(fileURL);
+            //set the state to the new url
+          /*  this.setState({
+              imageUrl: fileURL
+            })
+*/
+          //Now that the imageUrl is ready, post the item to the database
+        //  this.post();
+          })
+        });
+      } else {
+        //if they don't have a file ready to be uploaded just post to database
+      //  this.post()
+      }
+    }
+
+    this.onDrop = (files) => {
+      if(files) {
+        this.setState({
+          fileReceived: true,
+          fileName: files[0].name,
+          files: files
+        });
+    }
   }
+}
 
   render () {
 
@@ -32,25 +86,29 @@ export default class Options extends Component {
     var settingsPage = (
         <div className="settings" style={{maxWidth: '20%'}}>
           <Dialog
-            title="My Information"
             actions={findUserActions}
             modal={true}
             open={this.props.appState.optionsShow}
+            contentStyle={{maxWidth: "500px"}}
           >
             <Divider />
             <div className="optionsContainer" style={{display:"flex", flexDirection:"row"}}>
               <div className="leftOptions" style={{flex: 3 }}>
+                <h2>My Information</h2>
                 <p>Username: {this.props.appState.currentUser.username}</p>
                 <p>Name: {this.props.appState.currentUser.firstName + ' ' + this.props.appState.currentUser.lastName}</p>
                 <p>Email: {this.props.appState.currentUser.email}</p>
-                <button>Change Password</button>
+                <FlatButton label="Change email" primary={true}/>
+                <FlatButton label="Change Password" primary={true}/>
               </div>
             <div className="rightOptions" style={{flex: 2}}>
-              <Paper style={{marginTop: 10, maxHeight: 150, textAlign:'center'}} zDepth={1} >
-                <img alt={''} style={{maxHeight: 150, maxWidth: '100%'}} src="http://res.cloudinary.com/getmeagiftlegacy/image/upload/v1510851221/most_normal_picture_m7n8rs.jpg"/>
+              <Paper style={{marginTop: 10, maxHeight: 180, textAlign:'center', maxWidth: 140}} zDepth={1} >
+                <img alt={''} style={{maxHeight: 180, maxWidth: '100%', minWidth:"100%"}} src="http://res.cloudinary.com/getmeagiftlegacy/image/upload/v1510851221/most_normal_picture_m7n8rs.jpg"/>
               </Paper>
-              <p>Add/Edit phot</p>
-              <p>Upload div here</p>
+              <Dropzone disableClick={false} multiple={false} accept={'image/*'} onDrop={this.onDrop} style={{maxHeight: 50, maxWidth: 150}}>
+                <FlatButton secondary label="Add New Photo"></FlatButton>
+              </Dropzone>
+              {this.state.fileReceived ? <span style={{color: 'blue', width: 150}}>{this.state.fileName}</span> : null}
             </div>
           </div>
           </Dialog>
@@ -59,7 +117,7 @@ export default class Options extends Component {
 
     if (this.props.appState.optionsShow){
       return (
-        <div>
+        <div className="settingOuterDiv">
         {settingsPage}
         </div>
       )
