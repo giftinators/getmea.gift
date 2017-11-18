023400f876d345342681b4da07131b259c974da0
Cecilia Goss <ceci.goss@gmail.com>
2017-11-17 18:21:37 -0600

    friends list component turned into class

--- client/src/FriendsList.js
+++ client/src/FriendsList.js
@@ -5,63 +5,64 @@ import Paper from 'material-ui/Paper';
 import React from 'react';
 import axios from 'axios';
 
+export default class FriendsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+      pending: []
+    };
+    this.retrieveFriends = this.retrieveFriends.bind(this);
+  }
 
+  componentWillMount() {
+    this.retrieveFriends();
+  }
 
-const FriendsList = (props) => {
-  console.table( props.userData.friends);
-  var pending = []
-  var friends = []
-  var obj = props.userData.friends
-  for ( var user in props.userData.friends ) {
-    console.log('USER DATA: ', obj[user].friendStatus );
-    if ( obj[user].friendStatus === 'pending' ) {
-      console.log('INSIDE PENDING');
-      axios.post('api/search', {
-        searchMethod:'id',
-        userInput: user
-      })
-      .then((foundUser) => {
-        console.log('USERS FOUND IN pendingList: ', foundUser);
-        pending.push(foundUser)
-      })
-      .catch((err) => {
-        console.log('Handle search error: ', err);
-      })
-    } else if ( obj[user].friendStatus === 'friend') {
-      axios.post('api/search', {
-        searchMethod:'id',
-        userInput: user
-      })
-      .then((foundUser) => {
-        console.log('USERS FOUND IN FriendsList: ', foundUser);
-        pending.push(foundUser)
-      })
-      .catch((err) => {
-        console.log('Handle search error: ', err);
-      })
+  retrieveFriends() {
+    var pending = [];
+    var friends = [];
+    var userDataFriends = this.props.userData.friends;
+    console.table(userDataFriends);
+    for (var user in userDataFriends) {
+      axios
+        .post('api/search', {
+          searchMethod: 'id',
+          userInput: user
+        })
+        .then(foundUser => {
+          if (userDataFriends[user].friendStatus === 'pending') {
+            pending.push(foundUser.data);
+          } else if (userDataFriends[user].friendStatus === 'friend') {
+            friends.push(foundUser.data);
+          }
+          this.setState({
+            pending: pending,
+            friends: friends
+          });
+        })
+        .catch(err => {
+          console.log('Handle search error: ', err);
+        });
     }
   }
-  return (
-    <div className="friends-list">
-      <Paper>
-        <AppBar
-          title="Pending"
-          iconElementLeft={<div></div>}
-          />
-          <MenuItem key="1" primaryText="it's me" />
-          <MenuItem key="2" primaryText="hello" />
-          <Divider />
-          <MenuItem key="3" primaryText="it's me" />
-          <AppBar
-          title="All Friends"
-          iconElementLeft={<div></div>}
-          />
-          <MenuItem key="4" primaryText="hello" />
-          <Divider />
-          <MenuItem key="5" primaryText="it's me" />
-      </Paper>
-    </div>
-  );
-};
 
-export default FriendsList;
+  render() {
+    return (
+      <div className="friends-list">
+        <Paper>
+          <AppBar title="Pending" iconElementLeft={<div />} />
+          {this.state.pending.map((pendingFriend, index) => {
+            return (
+              <MenuItem key={'pending_' + index} primaryText={pendingFriend.firstName + ' ' + pendingFriend.lastName} />
+            );
+          })}
+          <AppBar title="All Friends" iconElementLeft={<div />} />
+          {this.state.friends.map((friend, index) => {
+            return <MenuItem key={'friend_' + index} primaryText={friend.firstName + ' ' + friend.lastName} />;
+          })}
+        </Paper>
+      </div>
+    );
+  }
+}
